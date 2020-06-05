0dafaec8f943cb4f2e5961cea7aea7333ae01e6c
Pavel <g79208776896@gmail.com>
2020-06-05 13:16:00 +0300

    "add header1, add top1"

---
+++ src/components/Top1.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="fon">
+        <div class="container">
+            <div class="header">
+                <div class="top__content">
+                    <h1 class="top__heading">New Automation
+                        Tool for Your </h1>
+                    <p class="text text_margin-bottom">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus tristique vulputate ultrices
+                        ut
+                        mauris
+                        tellus at. Posuere sollicitudin odio tellus elit.
+                    </p>
+                    <a href="#" class="button">See Our Project</a>
+                </div>
+
+            </div>
+            <img src="../assets/illustration.png">
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Top1"
+    }
+</script>
+
+<style scoped>
+
+    img {
+        padding-top: 150px;
+    }
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        max-width: 1200px;
+        padding-left: 30px;
+        padding-right: 30px;
+        margin: 0 auto;
+    }
+
+    .fon {
+        background: linear-gradient(0deg, #EBF3FB 8.84%, rgba(152, 195, 232, 0) 31.12%);
+        height: 728px;
+    }
+
+    .header {
+
+        padding-top: 34px;
+    }
+
+    .top__content {
+        width: 504px;
+        margin-top: 107px;
+        text-align: start;
+    }
+
+    .top__heading {
+        font-weight: 300;
+        font-size: 64px;
+        line-height: 74px;
+        color: #1F3F68;
+        margin-bottom: 32px;
+    }
+
+    .text {
+        font-size: 16px;
+        line-height: 26px;
+        letter-spacing: 0.05em;
+        color: #1F3F68;
+        opacity: 0.6;
+    }
+
+    .text_margin-bottom {
+        margin-bottom: 71px;
+    }
+
+    .button {
+        font-size: 16px;
+        line-height: 26px;
+        text-align: center;
+        letter-spacing: 0.04em;
+        color: #FFFFFF;
+        background: #5A98D0;
+        box-shadow: 5px 10px 20px rgba(53, 110, 173, 0.2);
+        border-radius: 10px;
+        padding: 15px 50px;
+        display: inline-block;
+        text-decoration: none;
+    }
+
+</style>
